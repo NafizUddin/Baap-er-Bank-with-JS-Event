2e3e8ec87757cd42bcfe0e4e5eec606f9de10237
NafizUddin <nafizuddin.ctg@gmail.com>
2023-08-15 19:18:26 +0600

    first commit

---
+++ Js files/login.js
@@ -0,0 +1,15 @@
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const userEmail = document.getElementById("user-email");
+  const email = userEmail.value;
+
+  const userPassword = document.getElementById("user-password");
+  const password = userPassword.value;
+
+  if (email.endsWith(".com") && password === "secret") {
+    console.log("Valid User");
+
+    window.location.href = "bank.html";
+  } else {
+    alert("Sorry wrong Input! You are an Invalid User");
+  }
+});
